729f67cd59c4676f6107ddd6a54f766f40637a89
test <test>
2021-09-17 08:31:01 +0800

    基于Java Config配置Ribbon【细粒度配置】（不推荐，似乎在新版本直接不支持这种方式了） 貌似2020.3的spring cloud不支持这种方式，Hoxton.SR6倒是支持 1、TestConfiguration.java指定服务对应的负载均衡策略 2、RibbonConfiguration.java自定义具体策略
    切换到了Hoxton.SR6才能用
    
    Hoxton.SR6需要我们重新配置yml分区的配置，不然yml分区会失效，当作不存在，直接执行最后的部分
    要改成这样
    spring:
      profiles: provider2
    
     参考：https://www.itmuch.com/spring-cloud/finchley-8/
     profile配置，yml分区

--- provider-user/src/main/java/com/demo/cloud/controller/UserController.java
+++ provider-user/src/main/java/com/demo/cloud/controller/UserController.java
@@ -2,9 +2,7 @@ package com.demo.cloud.controller;
 
 import com.demo.cloud.mapper.UserMapper;
 import com.demo.cloud.model.User;
-import com.netflix.discovery.DiscoveryClient;
-import com.netflix.discovery.DiscoveryManager;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
