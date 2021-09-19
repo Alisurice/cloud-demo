5f3c6477e7ba8777321dc22f80b31bf993c90840
test <test>
2021-09-19 16:45:18 +0800

    本地git配置中心
    报错正常：Caused by: org.eclipse.jgit.errors.CheckoutConflictException: Checkout conflict with files:
    因为还有些文件没有提交

--- config-client/src/main/java/com/demo/cloud/ConfigClientController.java
+++ config-client/src/main/java/com/demo/cloud/ConfigClientController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConfigClientController {
-    //@Value("${server.port}")
-    @Value("${profile}")
+    @Value("${server.port}")
+    //@Value("${profile}")
 
     private String profile;
 
