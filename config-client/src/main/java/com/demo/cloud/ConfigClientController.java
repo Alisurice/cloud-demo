9084fea05c83782bf07e364ed754e2842c3a312d
test <test>
2021-09-19 16:32:12 +0800

    异地git配置中心 pattern描述的是对于具有一定模式的application}-{profile，应该从哪个url获取配置文件 所以，client本来怎么写的，现在还是怎么写，不需要加任何的前缀后缀什么的，pattern是用来筛选application.name和profile的
    {application}-{profile}
    pattern: special*/dev*，就是要筛选出application为‘special*’，profile为‘dev*’的那些client，然后到该repos的uri中去加载配置文件，加载规则仍然是{application}-{profile}.yml

--- config-client/src/main/java/com/demo/cloud/ConfigClientController.java
+++ config-client/src/main/java/com/demo/cloud/ConfigClientController.java
@@ -2,15 +2,18 @@ package com.demo.cloud;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConfigClientController {
-    @Value("${server.port}")
+    //@Value("${server.port}")
+    @Value("${profile}")
+
     private String profile;
 
     @GetMapping("/profile")
-    public String hello() {
+    public @ResponseBody String hello() {
         return this.profile;
     }
 }
